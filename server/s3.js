ebe711aee0875dff04018b3fe70502be48e53f41
Michael <streiner89@gmail.com>
2022-10-07 02:55:06 +0200

    used custom env variables

--- server/s3.js
+++ server/s3.js
@@ -1,17 +1,18 @@
-import dotenv from 'dotenv'
 import aws from 'aws-sdk'
 import crypto from 'crypto'
 import { promisify } from "util"
+
 const randomBytes = promisify(crypto.randomBytes)
-
-dotenv.config()
-
-const bucketName = "opencanyon-images"
-const region = "eu-central-1"
+const bucketName = process.env.BUCKET_NAME
+const region = process.env.AWS_REGION
+const accessKeyId = process.env.ACCESS_KEY_ID
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
 
 const S3Config = {
   region: region,
-  bucketName: bucketName
+  bucketName: bucketName,
+  accessKeyId: accessKeyId,
+  secretAccessKey: secretAccessKey
 }
 
 aws.config.update(S3Config)
